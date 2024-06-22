955f838ddb0dcd1cc8eadb0ab7443edfda5c29d7
yizak223 <139069183+yizak223@users.noreply.github.com>
2024-06-22 23:51:44 +0300

    add shekel icon

--- frontend/src/components/Coins&Share/CoinsAndShare.jsx
+++ frontend/src/components/Coins&Share/CoinsAndShare.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import styles from './CoinsAndShare.module.css'
-import { FaBitcoin, FaEthereum } from "react-icons/fa";
+import { FaBitcoin, FaEthereum, FaShekelSign } from "react-icons/fa";
 
 export default function CoinsAndShare() {
     const [shekel, setShekel] = useState(0)
@@ -48,7 +48,7 @@ export default function CoinsAndShare() {
             <div className={styles.CoinsAndShareContainer}>
                 <div className={styles.iconAndType}>
                     <div className={styles.icon}>
-                        <i class="fa-solid fa-money-bill-transfer"></i>
+                        <FaShekelSign />
                     </div>
                     <div className={styles.type}>
                         <p className={styles.coin}>Shekel</p>
