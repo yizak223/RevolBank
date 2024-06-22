95c6984824415035ee2cf9e9165d1fddb2afc405
yizak223 <139069183+yizak223@users.noreply.github.com>
2024-06-22 23:49:58 +0300

    add bitcoin icon

--- frontend/src/components/Coins&Share/CoinsAndShare.jsx
+++ frontend/src/components/Coins&Share/CoinsAndShare.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import styles from './CoinsAndShare.module.css'
+import { FaBitcoin } from "react-icons/fa";
 
 export default function CoinsAndShare() {
     const [shekel, setShekel] = useState(0)
@@ -60,7 +61,7 @@ export default function CoinsAndShare() {
             <div className={styles.CoinsAndShareContainer}>
                 <div className={styles.iconAndType}>
                     <div className={styles.icon}>
-                        <i class="fa-solid fa-money-bill-transfer"></i>
+                        <FaBitcoin />
                     </div>
                     <div className={styles.type}>
                         <p className={styles.coin}>Bitcoin</p>
