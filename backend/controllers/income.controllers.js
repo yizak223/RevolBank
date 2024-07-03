bb796c85a4aecbf28f07523655bed3fb55f56950
yizak223 <139069183+yizak223@users.noreply.github.com>
2024-07-03 18:17:07 +0300

    fix the create income every month

--- backend/controllers/income.controllers.js
+++ backend/controllers/income.controllers.js
@@ -12,29 +12,35 @@ const getIncomes = async (req, res) => {
         return res.status(400).send(err);
     }
 }
-const createIncomes =async()=>{
+const createIncomes = async () => {
     try {
-        const accounts = await Account.find();
-        // const date = new Date(2024, 2, 1, 14, 58, 40);
+        // Schedule the job to run at midnight on the 1st of every month
         schedule.scheduleJob('0 0 1 * *', async function() {
-            for (const account of accounts) {
-                try {
-                    const income = new Income({
-                        idAccount: account._id,
-                        amount: 0,
-                        month: new Date().getMonth() + 1
-                    });
-                    await income.save();
-                } catch (err) {
-                    console.error(`Error creating income for account ${account._id}:`, err);
+            try {
+                const accounts = await Account.find(); 
+                for (const account of accounts) {
+                    try {
+                        const income = new Income({
+                            idAccount: account._id,
+                            amount: 0,
+                            month: new Date().getMonth() + 1
+                        });
+                        await income.save();
+                    } catch (err) {
+                        console.error(`Error creating income for account ${account._id}:`, err);
+                    }
                 }
+                console.log('Income entries created for all users at the start of the month.');
+            } catch (err) {
+                console.error('Error retrieving user accounts within the scheduled job:', err);
             }
-            console.log('Income entries created for all users at the start of the month.');
         });
+
+        console.log('Job scheduled to run at midnight on the 1st of every month.');
     } catch (err) {
-        console.error('Error retrieving user accounts:', err);
+        console.error('Error scheduling the job:', err);
     }
-}
+};
 createIncomes()
 const editIncomes = async (req, res) => {
     const id = req.params.id;
