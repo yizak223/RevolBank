fc2b3dec457f1c12933b0a0460d1f256be1de6f1
yizak223 <139069183+yizak223@users.noreply.github.com>
2024-07-04 13:58:19 +0300

    add tool hover and clean code

--- frontend/src/components/RecentTransaction/transaction.jsx
+++ frontend/src/components/RecentTransaction/transaction.jsx
@@ -1,36 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import styles from './RecentTransaction.module.css'
+import React, { useEffect, useState } from 'react';
+import styles from './RecentTransaction.module.css';
 import axios from 'axios';
 import baseUrl from '../../config/BaseUrl';
 import { formatDateTime2 } from '../../config/dateFormat';
 
 export default function Transaction({ transfer, token }) {
+    const [names, setNames] = useState({ nameTo: '', nameFrom: '' });
 
-    const [nameTo, setNameTo] = useState()
-    const [nameFrom, setNameFrom] = useState()
-
-    const fetchNameTo = async () => {
-        try {
-            const resNameTo = await axios.get(`${baseUrl}/accounts?_id=${transfer.from}`, {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            })
-            const resNameFrom = await axios.get(`${baseUrl}/accounts?_id=${transfer.to}`, {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            })
-            setNameTo(resNameTo.data.accounts[0].fullName)
-            setNameFrom(resNameFrom.data.accounts[0].fullName)
-        } catch (err) {
-            console.log(err)
-        }
-    }
-    
     useEffect(() => {
-        fetchNameTo()
-    }, [])
+        const fetchNames = async () => {
+            try {
+                const [resNameTo, resNameFrom] = await Promise.all([
+                    axios.get(`${baseUrl}/accounts?_id=${transfer.from}`, {
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        }
+                    }),
+                    axios.get(`${baseUrl}/accounts?_id=${transfer.to}`, {
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        }
+                    })
+                ]);
+                setNames({
+                    nameTo: resNameTo.data.accounts[0].fullName,
+                    nameFrom: resNameFrom.data.accounts[0].fullName
+                });
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        fetchNames();
+    }, [token, transfer.from, transfer.to]);
+
+    const isExpenditure = transfer.type === 'expenditure';
 
     return (
         <div className={styles.TransactionContainer}>
@@ -39,14 +42,21 @@ export default function Transaction({ transfer, token }) {
                     <i className="fa-solid fa-money-bill-transfer"></i>
                 </div>
                 <div className={styles.type}>
-                    <p className={`${styles.whereBuy} ${transfer.type === 'expenditure' ? styles.expenditure : styles.income}`}>{transfer.type}</p>
-                    <p className={styles.typeBuy}>{transfer.type === 'expenditure' ? nameFrom : nameTo}</p>
+                    <p className={`${styles.whereBuy} ${isExpenditure ? styles.expenditure : styles.income}`}>
+                        {transfer.type}
+                        <span className={styles.tool}>
+                            {isExpenditure ? 'Transfer you made' : 'You received a transfer'}
+                        </span>
+                    </p>
+                    <p className={styles.typeBuy}>{isExpenditure ? names.nameFrom : names.nameTo}</p>
                 </div>
             </div>
             <div className={styles.amountAndDate}>
-                <p className={`${styles.howMuch} ${transfer.type === 'expenditure' ? styles.expenditure : styles.income}`}>{transfer.type === 'expenditure' ? '-' : '+'} $ {transfer.amount} </p>
+                <p className={`${styles.howMuch} ${isExpenditure ? styles.expenditure : styles.income}`}>
+                    {isExpenditure ? '-' : '+'} $ {transfer.amount}
+                </p>
                 <p className={styles.typeBuy}>{formatDateTime2(transfer.createdAt)}</p>
             </div>
         </div>
-    )
+    );
 }
