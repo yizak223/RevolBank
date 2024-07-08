386d279cda3d8b144133074897026c27b30e2b06
yizak223 <139069183+yizak223@users.noreply.github.com>
2024-07-08 18:01:39 +0300

    wrong change in the base url deployment

--- frontend/src/config/BaseUrl.js
+++ frontend/src/config/BaseUrl.js
@@ -1,6 +1,5 @@
-const baseUrl = 'http://localhost:3115/api/v1'
+const baseUrl = 'https://revolbank-2.onrender.com/api/v1'
 
 export default baseUrl
 
-//
-//https://revolbank-2.onrender.com/api/v1
+//http://localhost:3115/api/v1
