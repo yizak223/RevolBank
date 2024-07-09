191df783328f96ecb6b3cc712cdafe1d19157171
yizak223 <139069183+yizak223@users.noreply.github.com>
2024-07-09 18:29:52 +0300

    final style for activities

--- frontend/src/pages/Balances/SortBy.jsx
+++ frontend/src/pages/Balances/SortBy.jsx
@@ -1,32 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './balnces.module.css';
 
 export default function SortBy() {
-    const [checked, setChecked] = useState(null);
-
-    const handleCheckboxChange = (value) => {
-        setChecked(value);
-    };
 
     return (
         <div className={styles.sortBy}>
-            <h3>Sort By</h3>
-            <label className={styles.checkboxContainer}>
-                <input 
-                    type="checkbox" 
-                    checked={checked === 'lastMonth'} 
-                    onChange={() => handleCheckboxChange('lastMonth')} 
-                />
-                Last Month
-            </label>
-            <label className={styles.checkboxContainer}>
-                <input 
-                    type="checkbox" 
-                    checked={checked === 'lastYear'} 
-                    onChange={() => handleCheckboxChange('lastYear')} 
-                />
-                Last Year
-            </label>
+            <h3 className={styles.sortByTitle}>Sort By</h3>
+            <h3 className={styles.activeSortBy}>Last Month</h3>
+            <h3 className={styles.sortByItem}>Last Year</h3>
         </div>
     );
 }
