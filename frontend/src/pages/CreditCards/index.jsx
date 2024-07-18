37c7ab8ded3b0fa4f3a1b4faa21a374a741d697c
yizak223 <139069183+yizak223@users.noreply.github.com>
2024-07-18 16:42:05 +0300

    animation responsive works at credir card page

--- frontend/src/pages/CreditCards/index.jsx
+++ frontend/src/pages/CreditCards/index.jsx
@@ -1,96 +1,134 @@
-import React, { useEffect, useState, useContext } from 'react'
+import React, { useEffect, useState, useContext } from 'react';
 import { useSelector } from 'react-redux';
 import CreateAccount from '../../components/CreateAccount';
-import SingleCard from '../../components/SingleCard'
-import Axios from 'axios'
-import { UserContext } from '../../context/User'
-import { AccountContext } from '../../context/Account'
-import BaseUrl from '../../config/BaseUrl'
-import CreateCard from '../../components/CreateCard'
-import ModalCreditCard from '../../components/ModalCreditCard'
-import styles from '../../components/About/about.module.css'
-import styles2 from '../Home/home.module.css'
-import styles3 from './creditCrad.module.css'
-
+import SingleCard from '../../components/SingleCard';
+import Axios from 'axios';
+import { UserContext } from '../../context/User';
+import { AccountContext } from '../../context/Account';
+import BaseUrl from '../../config/BaseUrl';
+import CreateCard from '../../components/CreateCard';
+import ModalCreditCard from '../../components/ModalCreditCard';
+import styles from './creditCrad.module.css';
 
 export default function CreditCard() {
   const modalAcount = useSelector((state) => state.modal.modalAcount);
-
-  const { token } = useContext(UserContext)
-  const { choosenAccount, accounts } = useContext(AccountContext)
-  const [cards, setCards] = useState([])
-  const [accountState, setAccountState] = useState(choosenAccount)
-  const [createCardMode, setCreateCardMode] = useState(false)
+  const { token } = useContext(UserContext);
+  const { choosenAccount, accounts } = useContext(AccountContext);
+  const [cards, setCards] = useState([]);
+  const [accountState, setAccountState] = useState(choosenAccount);
+  const [createCardMode, setCreateCardMode] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
-  const [card, setcard] = useState([])
-  const [creditCard, setCreditCard] = useState({
-    idAccount: accountState
-  })
+  const [card, setCard] = useState([]);
+  const [creditCard, setCreditCard] = useState({ idAccount: accountState });
+  const [scrollIndex, setScrollIndex] = useState(0);
+  const [cardWidth, setCardWidth] = useState(0); 
 
-  const createAccountStyle = false
+  const createAccountStyle = false;
 
   const fetchData = async () => {
     try {
-      const idAccount = choosenAccount?._id
-      const res = await Axios.get(`${BaseUrl}/crditCard?idAccount=${idAccount}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        })
-      console.log(res.data);
+      const idAccount = choosenAccount?._id;
+      const res = await Axios.get(`${BaseUrl}/crditCard?idAccount=${idAccount}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
       const updatedCards = res.data.creditCards.filter(card => card.isActive !== false);
       setCards(updatedCards);
     } catch (err) {
       console.error('There was a problem with the fetch operation:', err);
     }
-  }
+  };
+
   const deleteCard = async (id) => {
     try {
       const res = await Axios.patch(`${BaseUrl}/crditCard/${id}`, { isActive: false }, {
         headers: {
           Authorization: `Bearer ${token}`
         }
-      })
-      const updatedCards = cards.filter(card => card._id != id);
+      });
+      const updatedCards = cards.filter(card => card._id !== id);
       setCards(updatedCards);
     } catch (err) {
       console.log(err);
     }
-  }
+  };
+
   const fetchModal = (transfer) => {
-    setcard(transfer)
-  }
+    setCard(transfer);
+  };
 
   const handleChange = (e) => {
     setAccountState(e.target.value);
-    setCreditCard({ ...creditCard, idAccount: e.target.value })
-  }
+    setCreditCard({ ...creditCard, idAccount: e.target.value });
+  };
 
   const submitHandler = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      const res = await Axios.post(`${BaseUrl}/crditCard`, creditCard, {
+      const res = await Axios.post(`${BaseUrl}/creditCard`, creditCard, {
         headers: {
           Authorization: `Bearer ${token}`
         }
       });
-      setCards([...cards, res.data])
-      setCreateCardMode(!createCardMode)
+      setCards([...cards, res.data]);
+      setCreateCardMode(!createCardMode);
     } catch (error) {
       console.error('Error creating account:', error);
     }
-  }
-  useEffect(() => {
-    fetchData()
-  }, [token, choosenAccount])
+  };
 
+  useEffect(() => {
+    fetchData();
+  }, [token, choosenAccount]);
+  useEffect(() => {
+    // Calculate card width based on viewport width
+    const updateCardWidth = () => {
+        const viewportWidth = window.innerWidth;
+        let newCardWidth;
+        if (viewportWidth <= 360) {
+            newCardWidth = viewportWidth * 0.79;
+        } else if (viewportWidth <= 389) {
+            newCardWidth = viewportWidth * 0.86;
+        } else if (viewportWidth <= 412) {
+            newCardWidth = viewportWidth * 0.77;
+        } else if (viewportWidth <= 429) {
+            newCardWidth = viewportWidth * 0.89;
+        } else if (viewportWidth <= 768) {
+            newCardWidth = viewportWidth * 0.78;
+        } else if (viewportWidth <= 1024) {
+            newCardWidth = viewportWidth * 0.35; // 45% of viewport width for tablet
+        } else if (viewportWidth <= 1700) {
+            newCardWidth = viewportWidth * 0.29; // 26% of viewport width for desktop
+        } else {
+            newCardWidth = viewportWidth * 0.285; // 20% of viewport width for large screens
+        }
+
+        setCardWidth(newCardWidth);
+    };
+
+    updateCardWidth();
+    window.addEventListener('resize', updateCardWidth);
+
+    return () => window.removeEventListener('resize', updateCardWidth);
+  }, []);
+
+  const handleLeftArrowClick = () => {
+    setScrollIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+  };
+
+  const handleRightArrowClick = () => {
+    setScrollIndex((prevIndex) => Math.min(prevIndex + 1, cards.length - 1));
+  };
+cards.forEach(element => {
+  
+});
   return (
     <>
       {modalAcount && <CreateAccount createAccountStyle={createAccountStyle} />}
-      
-      <div className={styles2.container}>
-        <div className={styles3.secContainer}>
+
+      <div className={styles.container}>
+        <div className={styles.secContainer}>
           <div className={styles.left}>
             {modalOpen && (
               <ModalCreditCard
@@ -98,44 +136,31 @@ export default function CreditCard() {
                 setOpenModal={setModalOpen}
               />
             )}
-            {cards?.map((card, i) => (
-              <div key={i}>
-                {card.isActive ?
-                  <div className={styles3.containerCard}>
-                    <div className={styles3.cardOption}>
-                      <div className={styles3.maximize}>
-                        <i onClick={() => {
-                          setModalOpen(true);
-                          fetchModal(card)
-                        }} className="fa-solid fa-maximize"></i>
-                      </div>
-                      <button className={styles.dltBtn} onClick={() => { deleteCard(card._id) }}>
-                        <i className="fa-sharp fa-solid fa-trash"></i>
-                      </button>
-                    </div>
-                    <SingleCard card={card} cards={cards} setCards={setCards} />
-                  </div>
-                  : null}
+            <div className={styles.cardArrows}>
+              <div className={styles.arrowL} onClick={handleLeftArrowClick}>
+                <i className="fa-solid fa-chevron-left"></i>
               </div>
-            ))}
-            {
-              cards.length < 2 ? <>
-                <button className={createCardMode ? styles3.red : styles3.green} onClick={() => { setCreateCardMode(!createCardMode) }}>{createCardMode ? 'cancel' : 'Create Card'}</button> </>
-                : null
-            }
-          </div>
-          <div className={styles3.right}>
-            {
-              createCardMode ?
-                <CreateCard submitHandler={submitHandler} handleChange={handleChange} accounts={accounts} />
-                : <img className={styles3.img} src="src/images/KB.png" alt="" />
-            }
+              <div className={styles.containerCards} >
+                {cards?.map((card, i) => (
+                  card.isActive ?
+                    <div key={i} className={styles.containerCard} style={{ transform: `translateX(-${scrollIndex * cardWidth}px)`}}>
+                      <SingleCard card={card} cards={cards} setCards={setCards} />
+                    </div>
+                    : null
+                ))}
+              </div>
+              
+              <div className={styles.arrowR} onClick={handleRightArrowClick}>
+                <i className="fa-solid fa-chevron-right"></i>
+              </div>
+            </div>
 
+            <div className={styles.sellingSentence}>
+              <h1>Enjoy unmatched rewards and exclusive perks with our premium credit card.</h1>
+            </div>
           </div>
         </div>
       </div>
     </>
-
-
-  )
+  );
 }
